273925c85a43597769fe815e7939783cd8f0dfbd
CHAN JUN PARK <wns0415@gmail.com>
2021-12-13 17:25:20 +0900

    Update hello-spring(mvc, sign up)

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,8 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -11,5 +13,41 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "spring!!");
         return "hello";
+    }
+
+    // mvc & template engine 실습
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+
+    //api 실습 - String으로 리턴하는 경우
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name; // hello +{name} view 거치지 않고 그대로 데이터를 보내줌
+    }
+
+    //api 실습 - 객체로 리턴하는 경우
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
